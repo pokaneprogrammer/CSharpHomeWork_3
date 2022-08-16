2c42f458018eef5f636c7a355418d29ba0b6c4eb
pokaneprogrammer <mishenko.s80@mail.ru>
2022-08-16 17:57:03 +0300

    Урок3Задача2

---
+++ Урок3Задача2/Program.cs
@@ -0,0 +1,52 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+Console.Clear();
+System.Console.WriteLine();
+
+int numPlan = 3;            // количество осей координат
+int min = -7;               // минимальное значение диапазона для выбора случайных чисел
+int max = 9;                // максимальное значение диапазона для выбора случайных чисел
+
+int[] GetPointCoords(int numPlan)
+{
+    int[] coords = new int[numPlan];
+    for (int i = 0; i < numPlan; i++)
+    {
+        coords[i] = new Random().Next(min, max);
+    }
+    return coords;
+}
+
+void PrintCoords(int[] array)
+{
+    System.Console.Write($"(");
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        System.Console.Write($"{array[i]}, ");
+    }
+    System.Console.WriteLine($"{array[array.Length - 1]})");
+}
+
+double FindDistance(int[] arr1, int[] arr2)
+{
+    int sum = 0;
+    for (int i = 0; i < arr1.Length; i++)
+    {
+        sum = sum + (arr1[i] - arr2[i]) * (arr1[i] - arr2[i]);
+    }
+    double result = Math.Sqrt(sum);
+    return result;
+}
+
+int[] PointA = GetPointCoords(numPlan);
+int[] PointB = GetPointCoords(numPlan);
+
+System.Console.Write($"Координаты точки А: ");
+PrintCoords(PointA);
+System.Console.Write($"Координаты точки B: ");
+PrintCoords(PointB);
+
+System.Console.WriteLine($"Расстояние между точками А и В: {FindDistance(PointA, PointB)}");
+System.Console.WriteLine();
